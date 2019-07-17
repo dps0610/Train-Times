668f89f70d528c199ff8a898139308c4cccc67f7
dps0610 <dps0610@hotmail.com>
2019-07-17 19:05:36 -0400

    added moment math to display correct times.

--- assets/app.js
+++ assets/app.js
@@ -20,8 +20,7 @@ $("#addTrainBtn").on("click", function(){
         trainName: $("#nameInput").val().trim(),
         trainDestination: $("#destinationInput").val().trim(),
         trainFrequency: $("#frequencyInput").val().trim(),
-        nextArrival: $("#nextArrivalInput").val().trim(),
-        minutesAway: $("#minutesAwayInput").val().trim(),
+        firstTrain: $("#firstTrainInput").val().trim(),
     };
 
     database.ref().push(addTrainInfo);
@@ -29,27 +28,48 @@ $("#addTrainBtn").on("click", function(){
     $("#nameInput").val("");
     $("#destinationInput").val("");
     $("#frequencyInput").val("");
-    $("#nextArrivalInput").val("");
-    $("#minutesAwayInput").val("");
+    $("firstTrainInput").val("");
+    
 });
 
 database.ref().on("child_added", function(childSnapshot){
-    //console.log(childSnapshot.val());
+    console.log(childSnapshot.val());
 
     var tName = childSnapshot.val().trainName;
     var tDestination = childSnapshot.val().trainDestination;
     var tFrequency = childSnapshot.val().trainFrequency;
-    var tNextArrival = childSnapshot.val().nextArrival;
-    var tMinutesAway = childSnapshot.val().minutesAway;
+    var tFirstTrain = childSnapshot.val().firstTrain;
+     
+    var currentTime = moment().format("hh:mm"); //created the current time in military time
+    var convertedFirstTrain = moment(tFirstTrain, "hh:mm").subtract(1, "years"); // converting user input time to military time and setting in the past 
+    var timeDifference = moment().diff(convertedFirstTrain, "minutes"); //finding the difference between the current time and the user input time we just set
+    var remainder = timeDifference%tFrequency; // taking the total number of minutes since the user input time and diving it by the frequency of train arrivals, taking the remainder that is left. 
+    var tMinutesAway = tFrequency - remainder; //subtracting the train frequency by the remainder we just got. 
+    var newArrivalcalc = moment().add(tMinutesAway, "minutes"); //taking current time and adding how many minutes away the next train is
+    var tNextArrival = moment(newArrivalcalc).format("hh:mm a"); //formatting the next arrival time
 
-    var newRow = $("<tr>").append(
-        $("<td>").text(tName),
-        $("<td>").text(tDestination),
-        $("<td>").text(tFrequency),
-        $("<td>").text(tNextArrival),
-        $("<td>").text(tMinutesAway),
-    );
 
-    $("<tbody>").append(newRow);
+    if (tMinutesAway <= 1){
+        var newRow = $("<tr>").append(
+            $("<td>").text(tName),
+            $("<td>").text(tDestination),
+            $("<td>").text(tFrequency),
+            $("<td>").text(tNextArrival),
+            $("<td>").text("Arriving Now"),
+        );        
+    } else {
+        var newRow = $("<tr>").append(
+            $("<td>").text(tName),
+            $("<td>").text(tDestination),
+            $("<td>").text(tFrequency),
+            $("<td>").text(tNextArrival),
+            $("<td>").text(tMinutesAway),
+        );
+    }
+
+   $(".train-info").append(newRow);
+
     
 });
+
+
